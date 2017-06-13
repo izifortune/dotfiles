4b394db44de2be359afc0011165a7f06d1e7dcf6
Fabrizio Fortunato <fortune@cortesconta.net>
2017-06-13 23:12:18 +0100

    cleanup and init of brew

---
+++ brew.sh
@@ -0,0 +1,45 @@
+# z hopping around folders
+brew install z
+
+# run this script when this file changes guy.
+brew install entr
+
+# github util. gotta love `hub fork`, `hub create`, `hub checkout <PRurl>`
+brew install hub
+
+
+# mtr - ping & traceroute. best.
+brew install mtr
+
+# allow mtr to run without sudo
+mtrlocation=$(brew info mtr | grep Cellar | sed -e 's/ (.*//') #  e.g. `/Users/paulirish/.homebrew/Cellar/mtr/0.86`
+sudo chmod 4755 $mtrlocation/sbin/mtr
+sudo chown root $mtrlocation/sbin/mtr
+
+
+# Install other useful binaries
+brew install the_silver_searcher
+brew install fzf
+
+brew install git
+brew install imagemagick --with-webp
+brew install node # This installs `npm` too using the recommended installation method
+brew install pv
+brew install rename
+brew install tree
+brew install zopfli
+brew install ffmpeg --with-libvpx
+
+brew install terminal-notifier
+
+brew install android-platform-tools
+brew install pidcat   # colored logcat guy
+
+brew install ncdu # find where your diskspace went
+
+brew install zsh
+
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+# Remove outdated versions from the cellar
+cat brew.list | xargs brew install
+brew cleanup
