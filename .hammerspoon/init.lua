d95d1c40dd184e579dca3e656e874e9740b1c0a1
Fabrizio Fortunato <fortune@cortesconta.net>
2020-06-19 12:32:02 +0100

    Added bindings for ctrl -> cmd

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -3,243 +3,46 @@ local cmd = {'cmd'}
 
 hs.window.animationDuration = 0
 
--------------------------------------------------
----- hyper d for left one half window
--------------------------------------------------
+-- CTRL TAB requires less effort
+hs.hotkey.bind({"ctrl"}, "tab", function()
+    hs.eventtap.keyStroke({"cmd"}, "tab")
+end)
 
---hs.hotkey.bind({"cmd", "alt"}, 'Left', function()
---    if hs.window.focusedWindow() then
---        local win = hs.window.focusedWindow()
---        local f = win:frame()
---        local screen = win:screen()
---        local max = screen:frame()
+-- For Firefox and Chrome map ctrl-c and ctrl-v to cmd-c and cmd-v
+function termMetaCombo (letter)
+  return hs.hotkey.new({"ctrl"}, letter, nil, function()
+    hs.eventtap.keyStroke({"cmd"}, letter)
+  end)
+end
 
---        f.x = max.x
---        f.y = max.y
---        f.w = max.w / 2
---        f.h = max.h
---        win:setFrame(f)
---    else
---        hs.alert.show("No active window")
---    end
---end)
+termBinds = {
+  termMetaCombo('c'),
+  termMetaCombo('v')
+}
 
--------------------------------------------------
----- hyper g for right one half window
--------------------------------------------------
+function enableBinds()
+  -- hs.console.printStyledtext("term focused")
+  for k,v in pairs(termBinds) do
+    v:enable()
+  end
+end
 
---hs.hotkey.bind({"cmd", "alt"}, 'Right', function()
---    if hs.window.focusedWindow() then
---        local win = hs.window.focusedWindow()
---        local f = win:frame()
---        local screen = win:screen()
---        local max = screen:frame()
+function disableBinds()
+  -- hs.console.printStyledtext("term unfocused")
+  for k,v in pairs(termBinds) do
+    v:disable()
+  end
+end
 
---        f.x = max.x + (max.w / 2)
---        f.y = max.y
---        f.w = max.w / 2
---        f.h = max.h
---        win:setFrame(f)
---    else
---        hs.alert.show("No active window")
---    end
---end)
+local wf=hs.window.filter
 
---hs.hotkey.bind({"cmd", "alt"}, 'down', function()
---    if hs.window.focusedWindow() then
---        local win = hs.window.focusedWindow()
---        local f = win:frame()
---        local screen = win:screen()
---        local max = screen:frame()
+wf_firefox = wf.new{'Firefox'}
+wf_firefox:subscribe(wf.windowFocused, enableBinds)
+wf_firefox:subscribe(wf.windowUnfocused, disableBinds)
 
---        f.y = max.y + (max.h / 2)
---        f.x = max.x
---        f.h = max.h / 2
---        f.w = max.w
---        win:setFrame(f)
---    else
---        hs.alert.show("No active window")
---    end
---end)
-
---hs.hotkey.bind({"cmd", "alt"}, 'up', function()
---    if hs.window.focusedWindow() then
---        local win = hs.window.focusedWindow()
---        local f = win:frame()
---        local screen = win:screen()
---        local max = screen:frame()
-
---        f.y = max.y
---        f.x = max.x
---        f.h = max.h / 2
---        f.w = max.w
---        win:setFrame(f)
---    else
---        hs.alert.show("No active window")
---    end
---end)
-
--------------------------------------------------
----- hyper f for fullscreen
--------------------------------------------------
-
---hs.hotkey.bind({"cmd", "alt"}, 'f', function()
---    if hs.window.focusedWindow() then
---        local win = hs.window.focusedWindow()
---        local f = win:frame()
---        local screen = win:screen()
---        local max = screen:frame()
-
---        f.x = max.x
---        f.y = max.y
---        f.w = max.w
---        f.h = max.h
---        win:setFrame(f)
---    else
---        hs.alert.show("No active window")
---    end
---end)
-
--------------------------------------------------
----- hyper r for top left one quarter window
--------------------------------------------------
-
---hs.hotkey.bind({"ctrl", "cmd"}, 'Left', function()
---    if hs.window.focusedWindow() then
---        local win = hs.window.focusedWindow()
---        local f = win:frame()
---        local screen = win:screen()
---        local max = screen:frame()
-
---        f.x = max.x
---        f.y = max.y
---        f.w = max.w / 2
---        f.h = max.h / 2
---        win:setFrame(f)
---    else
---        hs.alert.show("No active window")
---    end
---end)
-
--------------------------------------------------
----- hyper t for top right one quarter window
--------------------------------------------------
-
---hs.hotkey.bind({"ctrl", "cmd"}, 'Right', function()
---    if hs.window.focusedWindow() then
---        local win = hs.window.focusedWindow()
---        local f = win:frame()
---        local screen = win:screen()
---        local max = screen:frame()
-
---        f.x = max.x + (max.w / 2)
---        f.y = max.y
---        f.w = max.w / 2
---        f.h = max.h / 2
---        win:setFrame(f)
---    else
---        hs.alert.show("No active window")
---    end
---end)
-
--------------------------------------------------
----- hyper v for bottom Right one quarter window
--------------------------------------------------
-
---hs.hotkey.bind({"ctrl", "cmd", "shift"}, 'Right', function()
---    if hs.window.focusedWindow() then
---        local win = hs.window.focusedWindow()
---        local f = win:frame()
---        local screen = win:screen()
---        local max = screen:frame()
-
---        f.x = max.x + (max.w / 2)
---        f.y = max.y + (max.h / 2)
---        f.w = max.w / 2
---        f.h = max.h / 2
---        win:setFrame(f)
---    else
---        hs.alert.show("No active window")
---    end
---end)
-
--------------------------------------------------
----- hyper c for bottom left one quarter window
--------------------------------------------------
-
---hs.hotkey.bind({"ctrl", "cmd", "shift"}, 'Left', function()
---    if hs.window.focusedWindow() then
---        local win = hs.window.focusedWindow()
---        local f = win:frame()
---        local screen = win:screen()
---        local max = screen:frame()
-
---        f.x = max.x
---        f.y = max.y + (max.h / 2)
---        f.w = max.w / 2
---        f.h = max.h / 2
---        win:setFrame(f)
---    else
---        hs.alert.show("No active window")
---    end
---end)
-
---hs.hotkey.bind(hyper, 'Left', function()
---  if hs.window.focusedWindow() then
---      local win = hs.window.focusedWindow()
---      win:moveOneScreenWest()
---  else
---      hs.alert.show("No active window")
---  end
---end)
-
---hs.hotkey.bind(hyper, 'Right', function()
---  if hs.window.focusedWindow() then
---      local win = hs.window.focusedWindow()
---      win:moveOneScreenEast()
---  else
---      hs.alert.show("No active window")
---  end
---end)
-
--------------------------------------------------
----- Reload config on write
--------------------------------------------------
-
---function reload_config(files)
---    hs.reload()
---end
---hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
---hs.alert.show("Config loaded")
-
-
--------------------------------------------------
----- Hyper hjkl to switch window focus
--------------------------------------------------
-
----- hs.hotkey.bind(hyper, 'k', function()
-----     if hs.window.focusedWindow() then
-----         hs.window.focusedWindow():focusWindowNorth()
-----     else
-----         hs.alert.show("No active window")
-----     end
----- end)
-
----- hs.hotkey.bind(hyper, 'j', function()
-----     if hs.window.focusedWindow() then
-----         hs.window.focusedWindow():focusWindowSouth()
-----     else
-----         hs.alert.show("No active window")
-----     end
----- end)
-
----- hs.hotkey.bind(hyper, 'l', function()
-----     if hs.window.focusedWindow() then
-----     hs.window.focusedWindow():focusWindowEast()
-----     else
-----         hs.alert.show("No active window")
-----     end
----- end)
+wf_chrome = wf.new{'Chrome'}
+wf_chrome:subscribe(wf.windowFocused, enableBinds)
+wf_chrome:subscribe(wf.windowUnfocused, disableBinds)
 
 ---- hs.hotkey.bind(hyper, 'h', function()
 ----     if hs.window.focusedWindow() then
@@ -274,27 +77,6 @@ hs.window.animationDuration = 0
 --  hs.eventtap.keyStroke({"cmd"}, "v")
 --end)
 
---local tiling = require "hs.tiling"
---local hotkey = require "hs.hotkey"
---local mash = {"ctrl", "cmd"}
-
---hotkey.bind(mash, "c", function() tiling.cycleLayout() end)
---hotkey.bind(mash, "j", function() tiling.cycle(1) end)
---hotkey.bind(mash, "k", function() tiling.cycle(-1) end)
---hotkey.bind(mash, "space", function() tiling.promote() end)
---hotkey.bind(mash, "f", function() tiling.goToLayout("fullscreen") end)
---hotkey.bind(mash, "r", function() tiling.retile() end)
-
---tiling.set('layouts', {
---  'fullscreen', 'gp-vertical'
---})
-
----- gp-vertical
----- If you want to set the layouts that are enabled
----- tiling.set('layouts', {
-----   'fullscreen', 'main-vertical'
----- })
-----
 
 
 hs.grid.setGrid('12x12') -- allows us to place on quarters, thirds and halves
